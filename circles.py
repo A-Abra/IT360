af5afe498def2ff15eee1787e1bf059230726790
A-Abra <apabraham472@gmail.com>
2023-10-02 19:48:26 -0400

    graphing a oval

--- circles.py
+++ circles.py
@@ -1,4 +1,5 @@
 import glfw
+import math
 from OpenGL.GL import *
 
 
@@ -32,7 +33,7 @@ if not glfw.init():
     exit()
 
 # Create a windowed mode window and its OpenGL context
-window = glfw.create_window(640, 480, "White Square on Black Background", None, None)
+window = glfw.create_window(640, 480, "Red Balls", None, None)
 if not window:
     glfw.terminate()
     exit()
@@ -48,17 +49,30 @@ glfw.set_key_callback(window, key_callback)
 while not glfw.window_should_close(window):
     # Clear the screen with black color
     # glClearColor(1.0, 1.0, 1.0, 1.0)
-    glClearColor(0.870, 0.905, 0.937, 1.0)
+    
+    width, height = glfw.get_window_size(window)
+    glViewport(0, 0, width, height)
+    
+    glClearColor(0.870, 0.905, 0.937, 1.0) # Set color to lightly salted lays chips blue
     glClear(GL_COLOR_BUFFER_BIT)
 
+    asp_ratio = width / height # tried to fix circle size with ratio
+
     # Draw a circle radius 1
-    radius = 1
-    glColor3f(0.807, 0.0, 0.0)  # Set color to white
-    glBegin(GL_QUADS)
-    glVertex2f(-0.5, -0.5)
-    glVertex2f(0.5, -0.5)
-    glVertex2f(0.5, 0.5)
-    glVertex2f(-0.5, 0.5)
+    x = float(0) 
+    y = float(0)
+    PINum = 3.14159265358979323846
+    radius = 0.2 * asp_ratio
+    triangleAmnt = int(100)
+    twicePi = float(2.0*PINum) # not multiplying results in half circle
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(0.807, 0.0, 0.0) # red circle color
+    glVertex2f(x,y)
+    for i in range(triangleAmnt + 1):
+        glVertex2f(
+            x + (radius * math.cos(i * twicePi / triangleAmnt)),
+            y + (radius * math.sin(i * twicePi / triangleAmnt))
+        )
     glEnd()
 
     # Draw a white square
